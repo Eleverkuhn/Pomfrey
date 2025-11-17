6e42916485294e30e4f7925fd5b0bc2db4e01f27
Eleverkhun <Eleverkuhn@users.noreply.github.com>
2025-11-17 14:40:11 +0300

    feat(setup): Add custom filter for INFO log messages. Move all logging classes to django-api/logger dir

---
+++ django-api/logger/setup.py
@@ -0,0 +1,22 @@
+import json, logging
+from logging import Logger
+from pathlib import Path
+
+
+class LoggingConfig:
+    path = Path("logger/logging_config.json")
+    logger_name = "pomfrey"
+
+    def load(self) -> None:
+        with open(self.path) as json_config:
+            config_file = json.load(json_config)
+        return config_file
+
+    def get_logger(self) -> Logger:
+        logger = logging.getLogger(self.logger_name)
+        return logger
+
+
+class OnlyInfoFilter(logging.Filter):
+    def filter(self, record):
+        return record.levelno == logging.INFO
--- django-api/utils.py
+++ django-api/utils.py
@@ -2,9 +2,6 @@
 This module provides various utilities for the project purposes
 """
 
-import json
-from pathlib import Path
-
 from psycopg import Cursor
 
 
@@ -13,12 +10,3 @@ class BaseDatabaseTest:
         cursor.execute("SELECT 1;")
         row = cursor.fetchone()
         self.assertEqual(row[0], 1)
-
-
-class LoggingConfig:
-    path = Path("logging_config.json")
-
-    def get(self) -> None:
-        with open(self.path) as json_config:
-            config_file = json.load(json_config)
-        return config_file
