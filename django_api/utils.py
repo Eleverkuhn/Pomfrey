73e027c2f6354547f4f80d5211161a628909ecb9
Eleverkhun <Eleverkuhn@users.noreply.github.com>
2025-11-18 17:55:04 +0300

    refactor(tests): Add 'BaseRegistryTest' which shares the same 'registry_data' dictionary between registry tests on different layers

--- django_api/utils.py
+++ django_api/utils.py
@@ -2,6 +2,7 @@
 This module provides various utilities for the project purposes
 """
 
+from django.test import TestCase
 from psycopg import Cursor
 
 
@@ -10,3 +11,12 @@ class BaseDatabaseTest:
         cursor.execute("SELECT 1;")
         row = cursor.fetchone()
         self.assertEqual(row[0], 1)
+
+
+class BaseRegistryTest(TestCase):
+    def setUp(self) -> None:
+        self.data = {
+            "email": "test@example.com",
+            "password": "test",
+            "confirm_password": "test"
+        }
