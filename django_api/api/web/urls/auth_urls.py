291294663f95831f5a857dd5fec276556850834e
Eleverkhun <Eleverkuhn@users.noreply.github.com>
2025-11-19 18:20:41 +0300

    feat(logout): Add knox built in Logout and LogoutAll endpoints

--- django_api/api/web/urls/auth_urls.py
+++ django_api/api/web/urls/auth_urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
+from knox.views import LogoutView, LogoutAllView
 
 from api.web.views.customer_views import Registry, Login
 
 urlpatterns = [
     path("registry/", Registry.as_view(), name="registry"),
     path("login/", Login.as_view(), name="login"),
+    path("logout/", LogoutView.as_view(), name="logout"),
+    path("logoutall/", LogoutAllView.as_view(), name="logout_all"),
 ]
--- django_api/pomfrey/urls.py
+++ django_api/pomfrey/urls.py
@@ -7,6 +7,5 @@ urlpatterns = [
     path("", Main.as_view()),
     path("admin/", admin.site.urls),
     path("auth/", include("api.web.urls.auth_urls")),
-    path("auth/", include("knox.urls")),
     path("my/", include("api.web.urls.customer_urls")),
 ]
