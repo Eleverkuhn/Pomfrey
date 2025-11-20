f1848701df05f614daf2cf1c62f55043eceee3cd
Eleverkhun <Eleverkuhn@users.noreply.github.com>
2025-11-20 18:00:36 +0300

    refactor(tests): Add a separate class for creation of customer test data. Add base test for Order entity

--- django_api/utils.py
+++ django_api/utils.py
@@ -5,14 +5,19 @@ This module provides various utilities for the project purposes
 import json
 from typing import override
 
+from faker import Faker
+from psycopg import Cursor
 from django.test import TestCase
 from rest_framework import status
 from rest_framework.response import Response
-from psycopg import Cursor
 from knox.models import AuthToken
 
+from logger.setup import LoggingConfig
 from api.service.base_services import BaseService
 from api.data.customer_data import Customer
+from api.data.pharmacy_data import (
+    PharmacyAddress, PharmacyWorkingSchedule, Pharmacy
+)
 
 
 class BaseDatabaseTest:
@@ -26,31 +31,23 @@ class UtilsTest:
     """
     A set of helping functions for setting up test data
     """
-    def _create_customer(self) -> Customer:
-        customer = Customer(email=self.data.get("email"))
-        customer.set_password(self.data.get("password"))
-        customer.save()
-        return customer
-
     def _create_auth_header(self) -> str:
-        auth_token = AuthToken.objects.create(self.customer)[1]
+        auth_token = AuthToken.objects.create(user=self.customer)[1]
         auth_header = f"Token {auth_token}"
         return auth_header
 
 
 class BaseAuthTest(TestCase):
     def setUp(self) -> None:
-        self.data = {
-            "email": f"test_{self._testMethodName}@example.com",
-            "password": "ComplicatedP@sSw0rd",
-        }
+        self.customer_generator = TestCustomerGenerator()
+        self.data = self.customer_generator.generate_login_data()
 
 
 class BaseTestWithCreatedCustomer(BaseAuthTest, UtilsTest):
     @override
     def setUp(self) -> None:
         super().setUp()
-        self.customer = self._create_customer()
+        self.customer = self.customer_generator.generate_customer()
 
 
 class BaseTestWithAuthenticationHeader(BaseTestWithCreatedCustomer):
@@ -89,3 +86,61 @@ class BaseViewTest:
         decoded_response = response.content.decode("utf-8")
         content = json.loads(decoded_response)
         return content
+
+
+class TestCustomerGenerator:
+    def __init__(self) -> None:
+        self.faker = Faker()
+
+    def generate_customer(self) -> Customer:
+        customer = self._create_customer()
+        return customer
+
+    def generate_login_data(self) -> dict[str, str]:
+        self.login_data = {
+            "email": self.faker.email(),
+            "password": self.faker.password(
+                length=10,
+                special_chars=True,
+                digits=True,
+                upper_case=True,
+                lower_case=True
+            )
+        }
+        return self.login_data
+
+    def _create_customer(self) -> Customer:
+        customer = Customer(email=self.login_data.get("email"))
+        customer.set_password(self.login_data.get("password"))
+        customer.save()
+        return customer
+
+
+class BaseOrderTest(BaseTestWithCreatedCustomer):
+    @override
+    def setUp(self) -> None:
+        super().setUp()
+        self.order_data = {
+            "customer": 1,
+            "pharmacy": 1,
+            "products": [1, 2, 3]
+        }
+        self.fake = Faker("en_US")
+        self.pharmacy = self._create_pharmacy()
+
+    def _create_order(self) -> None:
+        pass
+
+    def _create_pharmacy(self) -> None:
+        pharmacy_address = PharmacyAddress.objects.create(
+            region=self.fake.state()[:PharmacyAddress._meta.get_field("region").max_length],
+            city=self.fake.city()[:PharmacyAddress._meta.get_field("city").max_length],
+            street=self.fake.street_address()[:PharmacyAddress._meta.get_field("street").max_length],
+            apartment=self.fake.secondary_address()[:PharmacyAddress._meta.get_field("apartment").max_length],
+            postal_code=self.fake.postcode()[:PharmacyAddress._meta.get_field("postal_code").max_length]
+        )
+        LoggingConfig().logger.debug("%r", pharmacy_address)
+        return pharmacy_address
+
+    def _cretat_products(self) -> None:
+        pass
