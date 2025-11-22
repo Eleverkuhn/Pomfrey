3f52217d62b6ffeddfe80f663c2c83c94688acfe
Eleverkhun <Eleverkuhn@users.noreply.github.com>
2025-11-22 18:02:11 +0300

    feat(pharmacy): Add repositories for 'Pharmacy' model and it's relations. Move all functionality related to 'Pharmacy' creation from 'utils.py' to 'pharmacy_data.py'

--- django_api/api/data/pharmacy_data.py
+++ django_api/api/data/pharmacy_data.py
@@ -1,9 +1,11 @@
-from typing import override
+from typing import override, Generic
 
 from django.db import models
 from phonenumber_field.modelfields import PhoneNumberField
 
-from api.data.base_data import FieldDefault
+from api.data.base_data import (
+    FieldDefault, BaseRepository, BaseRelationRepository, ModelType
+)
 
 
 class Pharmacy(models.Model):
@@ -33,7 +35,7 @@ class PharmacyAddress(models.Model):
     @override
     def __repr__(self) -> str:
         address_parts = [
-            self.pharmacy.id,
+            str(self.pharmacy.id),
             self.region,
             self.city,
             self.street,
@@ -52,3 +54,72 @@ class PharmacyWorkingSchedule(models.Model):
 
     class Meta:
         db_table = "pharmacy_working_schedules"
+
+
+class PharmacyRepository(BaseRepository):
+    @override
+    def __init__(
+            self, model_data: dict, model: type[Pharmacy] = Pharmacy
+    ) -> None:
+        super().__init__(model, model_data)
+
+    @property
+    def address_repository(self) -> "PharmacyRelationRepository":
+        address_repository = PharmacyRelationRepository(
+            PharmacyAddress, self.model_data["address"], self.pharmacy
+        )
+        return address_repository
+
+    @property
+    def schedule_repository(self) -> "PharmacyWorkingScheduleRepository":
+        schedule_repository = PharmacyWorkingScheduleRepository(
+            self.model_data["schedule"], self.pharmacy
+        )
+        return schedule_repository
+
+    @override
+    def create(self) -> Pharmacy:
+        self.pharmacy = self.model(**self.model_data["pharmacy"])
+        self.pharmacy.save()
+        self._create_relations()
+        return self.pharmacy
+
+    def _create_relations(self) -> None:
+        self.address_repository.create()
+        self.schedule_repository.create()
+
+
+class PharmacyRelationRepository(
+        BaseRelationRepository[ModelType], Generic[ModelType]
+):
+    related_field_name = "pharmacy"
+
+    @override
+    def __init__(
+            self,
+            model: type[ModelType],
+            model_data: dict,
+            related_model: Pharmacy,
+            related_field_name: str = "pharmacy"
+    ) -> None:
+        super().__init__(model, model_data, related_model, related_field_name)
+
+
+class PharmacyWorkingScheduleRepository(
+        PharmacyRelationRepository[PharmacyWorkingSchedule]
+):
+    @override
+    def __init__(
+            self,
+            model_data: dict,
+            related_model: Pharmacy,
+            model: type[PharmacyWorkingSchedule] = PharmacyWorkingSchedule
+    ) -> None:
+        super().__init__(model, model_data, related_model)
+
+    @override
+    def create(self) -> PharmacyWorkingSchedule:
+        model = self.model(**self.model_data)
+        model.save()
+        model.pharmacies.set([self.related_model])
+        return model
