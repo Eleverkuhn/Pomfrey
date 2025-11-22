633487295d54399343218adddb1fd23ded50900b
Eleverkhun <Eleverkuhn@users.noreply.github.com>
2025-11-22 13:51:23 +0300

    feat(order): Add generic base class for 'Repository' entity. Repository represents set of encapsulated Django ORM methods for a specific model

--- django_api/api/data/base_data.py
+++ django_api/api/data/base_data.py
@@ -1,3 +1,10 @@
+from typing import Generic, TypeVar
+
+from django.db import models
+
+ModelType = TypeVar("ModelType", bound=models.Model)
+
+
 class FieldDefault:
     nanoid_size: int = 10
     phone_lenght: int = 10
@@ -8,3 +15,14 @@ class FieldDefault:
     street_length: int = 30
     apartment_length: int = 15
     postal_code_length: int = 8
+
+
+class BaseRepository(Generic[ModelType]):
+    def __init__(self, model: type[ModelType], model_data: dict) -> None:
+        self.model: type[ModelType] = model
+        self.model_data = model_data
+
+    def create(self) -> ModelType:
+        model = self.model(**self.model_data)
+        model.save()
+        return model
