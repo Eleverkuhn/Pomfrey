d0452abc5d35d0b636fd550b15543b64033f4912
Eleverkhun <Eleverkuhn@users.noreply.github.com>
2025-11-22 10:18:09 +0300

    Add 'status' field to 'Order' model. Add 'Delivery' and 'Payment' models

--- django_api/api/data/order_data.py
+++ django_api/api/data/order_data.py
@@ -8,9 +8,15 @@ from api.data.pharmacy_data import Pharmacy
 
 
 class Order(models.Model):
+    class Status(models.TextChoices):
+        PENDING = "Pending"
+        RECEIVED = "Received"
+        CANCELLED = "Cancelled"
+
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
     products = models.ManyToManyField(Product)
     pharmacy = models.ForeignKey(Pharmacy, on_delete=models.CASCADE)
+    status = models.CharField(choices=Status, default=Status.PENDING)
 
     @override
     def __repr__(self) -> str:
@@ -19,3 +25,45 @@ class Order(models.Model):
 
     class Meta:
         db_table = "orders"
+
+
+class Delivery(models.Model):
+    class Type(models.TextChoices):
+        PICKUP = "Pickup"
+        DELIVERY = "Delivery"
+
+    class Status(models.TextChoices):
+        PROCESSING = "Processing"
+        HANDOVER = "Handover to delivery"
+        IN_TRANSIT = "In transit"
+        READY = "Ready for pickup"
+        RECEIVED = "Received"
+
+    type = models.CharField(choices=Type)
+    status = models.CharField(choices=Status)
+
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+
+    @override
+    def __repr__(self) -> str:
+        repr = f"{self.id}, {self.type}, {self.status}, {self.order.customer}"
+        return repr
+
+    class Meta:
+        db_table = "deliveries"
+
+
+class Payment(models.Model):
+    class Type(models.TextChoices):
+        CARD = "Card"
+        CASH = "Cash"
+
+    type = models.CharField(choices=Type)
+    is_paid = models.BooleanField(default=False)
+
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+
+    @override
+    def __repr__(self) -> str:
+        repr = f"{self.id}, {self.type}, {self.is_paid}, {self.order.customer}"
+        return repr
