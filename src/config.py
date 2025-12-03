945f39618ac4d9b2f65001a99861a07ad98c23e6
Eleverkhun <Eleverkuhn@users.noreply.github.com>
2025-12-03 15:08:18 +0300

    feat(project): Add distinct env variables for local and docker

--- src/config.py
+++ src/config.py
@@ -1,23 +1,31 @@
+import os
+
 from pathlib import Path
 
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
-env_path = str(Path(__file__).parent.parent / ".env")
+
+def get_env_file() -> str:
+    dir = Path(__file__).parent.parent
+    if os.getenv("TEST_ENV") == "docker":
+        return str(dir / ".env.docker")
+    return str(dir / ".env.local")
 
 
 class Settings(BaseSettings):
+    test_env: str
+
     postgres_user: str
     postgres_password: str
     postgres_db: str
     postgres_host: str
-    postgres_container_host: str
     postgres_port: str
 
     django_host: str
     django_port: str
     django_key: str
 
-    model_config = SettingsConfigDict(env_file=env_path)
+    model_config = SettingsConfigDict(env_file=get_env_file())
 
 
 settings = Settings()
